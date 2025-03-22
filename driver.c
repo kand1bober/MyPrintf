5dc75ac8eb1a822edd33ab53e0a8385194fd3276
kand1bober <kurnevich.slava@gmail.com>
2025-03-22 04:25:39 +0300

    add readme

--- driver.c
+++ driver.c
@@ -6,44 +6,6 @@ int main( int argc, char* argv[] )
 {
     int a = 0;
 
-    // a = MyPrintf("ASCII Table:\n\n"
-    //              "hex  oct   dec  char\n\n");
-
-    // for( char i = 'a'; i <= 'z'; i++ )
-    // {
-    //     a = MyPrintf("%x = %o = %d = %c\n", (int)i, (int)i, (int)i, i );
-    // }
-
-    // a = MyPrintf("%d %c %o %b \n"
-    //             "My uncle was a man of virture, \n"
-    
-    //             "When he became quite old and sick,\n"
-
-    //             "He sought respect and tried to teach me,\n"
-
-    //             "His only heir, verte and weak.\n"
-
-    //             "He had the fun, I had the sore,\n"
-
-    //             "But grecious goodness! what a bore!\n"
-
-    //             "To sit by bedplace day and night,\n"
-
-    //             "Not doing even step aside,\n"
-
-    //             "And what a cheep and cunning thing\n"
-
-    //             "To entertain the sad,\n"
-
-    //             "To serve around, make his bed,\n"
-
-    //             "To fetch the pills, to mourn and grim,\n"
-
-    //             "To sigh outloud, think along:\n"
-
-    //             "God damn old man, why ain't you gone?'\n",
-    //             -1313, 90, 235, 1313 );
-
     printf("%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b\n",
     -1,
     -1,
